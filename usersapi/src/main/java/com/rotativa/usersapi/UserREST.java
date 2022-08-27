e052ea4ed15b117daa3118fce22bf7b6e02bf0de
Ezequiel Sousa <63311919+Escowolf@users.noreply.github.com>
2022-08-27 08:04:02 -0300

    CORS CONFIGURE

--- usersapi/src/main/java/com/rotativa/usersapi/UserREST.java
+++ usersapi/src/main/java/com/rotativa/usersapi/UserREST.java
@@ -3,6 +3,7 @@ package com.rotativa.usersapi;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,8 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.rotativa.usersapi.database.RepositorioUser;
 import com.rotativa.usersapi.entidades.User;
-
-//possui as operações operações básicas para manipular o usuário no bd
+/**
+ * possui as operações operações básicas para manipular o usuário no bd
+ * */
+@CrossOrigin(origins = "http://localhost:3000") //Permite que o back envie e receba dados de uma origem diferente
 @RestController
 @RequestMapping("/user")
 public class UserREST {
