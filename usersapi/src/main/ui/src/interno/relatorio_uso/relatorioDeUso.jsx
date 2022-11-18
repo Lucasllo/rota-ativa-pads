dcda307dff547dadf7253fe7f5549574b6cd2b8b
Lucas Lopes <lucaslopesoliveira@gmail.com>
2022-11-18 13:02:20 +0000

    corrigindo cadastro de vagas

--- usersapi/src/main/ui/src/interno/relatorio_uso/relatorioDeUso.jsx
+++ usersapi/src/main/ui/src/interno/relatorio_uso/relatorioDeUso.jsx
@@ -22,7 +22,7 @@ export function RelatorioDeUso() {
   const [dados, setDados] = useState([]);
 
   const navigate = useNavigate();
-  
+
   const vagaService = new VagaService();
 
   const [buscar, setBuscar] = useState("");
@@ -32,37 +32,37 @@ export function RelatorioDeUso() {
   const [lista, setLista] = useState([]);
 
   function testaBusca(nome) {
-    const regex = new RegExp(buscar, 'i');
+    const regex = new RegExp(buscar, "i");
     return regex.test(nome);
-  } 
+  }
 
   useEffect(() => {
-      const novaLista = vagas.filter(item => testaBusca(item.rua_avenida) || testaBusca(item.Bairro))
-      setLista(novaLista);
-  }, [buscar])
+    const novaLista = vagas.filter(
+      (item) => testaBusca(item.rua_avenida) || testaBusca(item.Bairro)
+    );
+    setLista(novaLista);
+  }, [buscar]);
 
   useEffect(() => {
     areasService.getAreas().then((resp) => {
       setDados(resp.data);
-    })
-  }, [])
-
+    });
+  }, []);
 
   const [currentPage, setCurrentPage] = useState(1);
 
   useEffect(() => {
-      vagaService.getVaga().then((resp) => {
-        setVagas(resp.data);
-        setLista(resp.data);
-      });
-  }, [])
+    vagaService.getVaga().then((resp) => {
+      setVagas(resp.data);
+      setLista(resp.data);
+    });
+  }, []);
 
   const currentTableData = useMemo(() => {
-      const firstPageIndex = (currentPage - 1) * PageSize;
-      const lastPageIndex = firstPageIndex + PageSize;
-      return lista.slice(firstPageIndex, lastPageIndex);
+    const firstPageIndex = (currentPage - 1) * PageSize;
+    const lastPageIndex = firstPageIndex + PageSize;
+    return lista.slice(firstPageIndex, lastPageIndex);
   }, [currentPage, lista]);
-
 
   const { isLoaded } = useJsApiLoader({
     googleMapsApiKey: "AIzaSyAQYGeShstIRAbsrS4lwyumbLwlG5t-sTA",
@@ -75,7 +75,6 @@ export function RelatorioDeUso() {
     return <SkeletonText />;
   }
 
-
   return (
     <>
       <div>
@@ -87,14 +86,22 @@ export function RelatorioDeUso() {
               mapContainerStyle={{ width: "100%", height: "100%" }}
             >
               {dados.map((d) => {
+                var x = vagas.find((p) => p.rua_avenida == d.rua_avenida);
 
-                var x = vagas.find((p) => p.rua_avenida == d.rua_avenida );
-                
                 return (
                   <>
                     <Polygon
-                      options={{ strokeColor: x?.usuarios?.length > 2 ? "#FF5858" : "#027373", strokeWeight: 2 }}
-                      onClick={()=>{navigate(`/menulogado/relatorioUsuarios/${x.id}`, {state: { vaga: x}, replace: true});}}
+                      options={{
+                        strokeColor:
+                          x?.usuarios?.length > 2 ? "#FF5858" : "#027373",
+                        strokeWeight: 2,
+                      }}
+                      onClick={() => {
+                        navigate(`/menulogado/relatorioUsuarios/${x.id}`, {
+                          state: { vaga: x },
+                          replace: true,
+                        });
+                      }}
                       paths={d.path}
                     ></Polygon>
                   </>
@@ -102,7 +109,10 @@ export function RelatorioDeUso() {
               })}
               {showInfoWindow && (
                 <Marker
-                  position={{ lat: -22.618827234831404, lng: -42.57636812499999 }}
+                  position={{
+                    lat: -22.618827234831404,
+                    lng: -42.57636812499999,
+                  }}
                 ></Marker>
               )}
             </GoogleMap>
@@ -110,67 +120,78 @@ export function RelatorioDeUso() {
         </Flex>
 
         <div className="card margem-relatorio">
-                <div className="card-body">
-                    <h1 className="py-3 text-center font-bold font-up blue-text">
-                        Vagas Mais Usadas
-                    </h1>
-                    <div className="input-group md-form form-sm form-2 pl-0">
-                      <input 
-                        className="form-control my-0 py-1 pl-3 purple-border" 
-                        type="text" 
-                        placeholder="Pesquise o endereço ou bairro aqui..." 
-                        aria-label="Search"
-                        value={buscar}
-                        onChange={evento => setBuscar(evento.target.value)}
-                      />
-                      <span className="input-group-addon waves-effect purple lighten-2" id="basic-addon1"><a><i className="fa fa-search white-text" aria-hidden="true"></i></a></span>
-                    </div>
-
-                    <table className="table table-hover table-responsive mb-0">
-                        <thead>
-                            <tr>
-                                <th scope="row">#</th>
-                                <th className="th-lg">Nome</th>
-                                <th className="th-lg">Crédito</th>
-                                <th className="th-lg">Total do veículos</th>
-                                <th className="th-lg">Tempo de uso</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {currentTableData.map((item) => {
-                                var tempo = 0;
-                                var credito = 0;
-                                var veiculos = 0;
-                                item.usuarios.forEach(element => {
-                                    tempo += element.tempo_uso
-                                    credito += element.credito
-                                    veiculos += element.veiculo.length
-                                })
-                                return (
-                                    <tr>
-                                        <td>{item.id}</td>
-                                        <td>
-                                            <Link to={{ pathname: `/menulogado/relatorioUsuarios/${item.id}` }}
-                                                state={{ vaga: item}}>
-                                                {item.rua_avenida} - {item.Bairro}
-                                            </Link>
-                                        </td>
-                                        <td>{credito}</td>
-                                        <td>{veiculos}</td>
-                                        <td>{tempo} h</td>
-                                    </tr>
-                                )
-                            })}
-                        </tbody>
-                    </table>
-                    <Pagination
-                        currentPage={currentPage}
-                        totalCount={lista.length}
-                        pageSize={PageSize}
-                        onPageChange={page => setCurrentPage(page)}
-                    />
-                </div>
+          <div className="card-body">
+            <h1 className="py-3 text-center font-bold font-up blue-text">
+              Vagas Mais Usadas
+            </h1>
+            <div className="input-group md-form form-sm form-2 pl-0">
+              <input
+                className="form-control my-0 py-1 pl-3 purple-border"
+                type="text"
+                placeholder="Pesquise o endereço ou bairro aqui..."
+                aria-label="Search"
+                value={buscar}
+                onChange={(evento) => setBuscar(evento.target.value)}
+              />
+              <span
+                className="input-group-addon waves-effect purple lighten-2"
+                id="basic-addon1"
+              >
+                <a>
+                  <i className="fa fa-search white-text" aria-hidden="true"></i>
+                </a>
+              </span>
             </div>
+
+            <table className="table table-hover table-responsive mb-0">
+              <thead>
+                <tr>
+                  <th scope="row">Nome</th>
+                  <th className="th-lg">Rua/Avenida - Bairro</th>
+                  <th className="th-lg">Crédito</th>
+                  <th className="th-lg">Total do veículos</th>
+                  <th className="th-lg">Tempo de uso</th>
+                </tr>
+              </thead>
+              <tbody>
+                {currentTableData.map((item) => {
+                  var tempo = 0;
+                  var credito = 0;
+                  var veiculos = 0;
+                  item.usuarios.forEach((element) => {
+                    tempo += element.tempo_uso;
+                    credito += element.credito;
+                    veiculos += element.veiculo.length;
+                  });
+                  return (
+                    <tr>
+                      <td>{item.nome_vaga}</td>
+                      <td>
+                        <Link
+                          to={{
+                            pathname: `/menulogado/relatorioUsuarios/${item.id}`,
+                          }}
+                          state={{ vaga: item }}
+                        >
+                          {item.rua_avenida} - {item.Bairro}
+                        </Link>
+                      </td>
+                      <td>{credito}</td>
+                      <td>{veiculos}</td>
+                      <td>{tempo} h</td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+            <Pagination
+              currentPage={currentPage}
+              totalCount={lista.length}
+              pageSize={PageSize}
+              onPageChange={(page) => setCurrentPage(page)}
+            />
+          </div>
+        </div>
       </div>
     </>
   );
